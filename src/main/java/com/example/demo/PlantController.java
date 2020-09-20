b82177d0b84509e221a5886adad7db28c69e1c04
Mark Szymanski <mark@blueye.com>
2020-09-20 10:42:05 -0400

    introduce plant repository

--- src/main/java/com/example/demo/PlantController.java
+++ src/main/java/com/example/demo/PlantController.java
@@ -6,32 +6,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PlantController {
-    // TODO: fill out remaining HTTP verbs (among other things...)
+    private final PlantRepository repository;
+
+    PlantController(PlantRepository repository) {
+        this.repository = repository;
+    }
 
     @GetMapping("/plants/{id}")
-    public String one(@RequestParam(value="id", defaultValue= "1") String id) {
+    public Plant one(@RequestParam(value="id", defaultValue= "1") String id) {
         // Pretend we have retrieved a plant from some data store matching that id
         // Then turn it into an appropriate textual representation to return to the client
         var myPlant = new Plant((long) 1,
-                "Trillum",
+                "Trillium",
                 "Grandiflorum",
                 "White Trillium",
                 "Beautiful");
 
-
-        // NB: all this is for illustrative purposes to motivate the use of
-        // Spring features in subsequent revisions. This is *not* how you would
-        // actually want to write services! : )
-        return String.format(
-                "{\"id\": \"%d\"," +
-                "\"genus\": \"%s\"," +
-                "\"species\": \"%s\"," +
-                "\"commonName\": \"%s\"," +
-                        "\"description\": \"%s\"",
-                myPlant.getId(),
-                myPlant.getGenus(),
-                myPlant.getSpecies(),
-                myPlant.getCommonName(),
-                myPlant.getDescription());
+        // Much better than the prior revision!
+        return myPlant;
     }
 }

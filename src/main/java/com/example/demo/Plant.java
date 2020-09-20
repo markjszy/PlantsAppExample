d3aa1f0624c7cb79e26e5391b309b751dd31baf1
Mark Szymanski <mark@blueye.com>
2020-09-19 23:36:48 -0400

    initial commit with a basic entity (Plant)

---
+++ src/main/java/com/example/demo/Plant.java
@@ -0,0 +1,43 @@
+package com.example.demo;
+
+public class Plant {
+    private Long id;
+    private String genus;
+    private String species;
+    private String commonName;
+    private String description;
+
+    public Plant() {}
+
+    public String getGenus() {
+        return genus;
+    }
+
+    public void setGenus(String genus) {
+        this.genus = genus;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getCommonName() {
+        return commonName;
+    }
+
+    public void setCommonName(String commonName) {
+        this.commonName = commonName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}

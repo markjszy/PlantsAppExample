e816b883a42136e0cda45d41a13312255f1516d5
Mark Szymanski <mark@blueye.com>
2020-09-20 13:01:47 -0400

    introduce basic HATEOAS implementation

--- src/main/java/com/example/demo/PlantController.java
+++ src/main/java/com/example/demo/PlantController.java
@@ -1,11 +1,12 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.CollectionModel;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class PlantController {
@@ -15,14 +16,49 @@ public class PlantController {
         this.repository = repository;
     }
 
+    // Aggregate operations
+
     @GetMapping("/plants")
-    List<Plant> all() {
-        return repository.findAll();
+    CollectionModel<EntityModel<Plant>> all() {
+        List<EntityModel<Plant>> plants = repository.findAll().stream()
+                .map(plant -> EntityModel.of(plant,
+                        linkTo(methodOn(PlantController.class).one(plant.getId())).withSelfRel(),
+                        linkTo(methodOn(PlantController.class).all()).withRel("plants")))
+                .collect(Collectors.toList());
+        return CollectionModel.of(plants, linkTo(methodOn(PlantController.class).all()).withSelfRel());
+    }
+
+    @PostMapping("/plants")
+    Plant newPlant(@RequestBody Plant newPlant) {
+        return repository.save(newPlant);
     }
 
     @GetMapping("/plants/{id}")
-    public Plant one(@PathVariable Long id) {
-        // Note: we will improve this situation with custom error handling in the next revision
-        return repository.findById(id).orElseThrow(() -> new PlantNotFoundException(id));
+    EntityModel<Plant> one(@PathVariable Long id) {
+        var plant = repository.findById(id).orElseThrow(() -> new PlantNotFoundException(id));
+        return EntityModel.of(plant,
+                linkTo(methodOn(PlantController.class).one(id)).withSelfRel(),
+                linkTo(methodOn(PlantController.class).all()).withRel("employees"));
+    }
+
+    @PutMapping("/plants/{id}")
+    Plant replacePlant(@RequestBody Plant newPlant, @PathVariable Long id) {
+        return repository.findById(id)
+                .map(plant -> {
+                    plant.setGenus(newPlant.getGenus());
+                    plant.setSpecies(newPlant.getSpecies());
+                    plant.setCommonName(newPlant.getCommonName());
+                    plant.setDescription(newPlant.getDescription());
+                    return repository.save(plant);
+                })
+                .orElseGet(() -> {
+                    newPlant.setId(id);
+                    return repository.save(newPlant);
+                });
+    }
+
+    @DeleteMapping("/plants/{id}")
+    void deletePlant(@PathVariable Long id) {
+        repository.deleteById(id);
     }
 }
